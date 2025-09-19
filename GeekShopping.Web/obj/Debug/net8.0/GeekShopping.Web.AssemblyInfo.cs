117ba49c51c84e3de1231b04ed4e36b4495985ca
Vanvrs <vanessrodrigues@hotmail.com>
2025-09-19 10:42:45 -0300

    gitignore

--- GeekShopping.Web/obj/Debug/net8.0/GeekShopping.Web.AssemblyInfo.cs
+++ GeekShopping.Web/obj/Debug/net8.0/GeekShopping.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f1782686dbad8dc21a51f8d97369d1ab72878c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
