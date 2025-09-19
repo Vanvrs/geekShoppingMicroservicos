c5f1782686dbad8dc21a51f8d97369d1ab72878c
Vanvrs <vanessrodrigues@hotmail.com>
2025-09-19 10:34:20 -0300

    Implementacao GeekShopping.web

---
+++ GeekShopping.Web/Program.cs
@@ -0,0 +1,33 @@
+﻿using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Configure HttpClient para ProductService - CORRIGIDO O NOME DA CHAVE
+builder.Services.AddHttpClient<IProductService, ProductService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])); // ← "ProductAPI" com I maiúsculo
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
