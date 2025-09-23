6e34d5882c044d7743acee6de638e6aad2bbaceb
Vanessa Rodrigues <vanessrodrigues@hotmail.com>
2025-09-23 16:54:31 -0300

    Implementação de Identity Server

--- GeekShooping/GeekShopping.Identity.Service/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GeekShopping.Identity.Service.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
